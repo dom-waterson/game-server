153cbacbe56ad819b80eb0e32e98f61d86e2c42c
Dominic Waterson <dominic.waterson@tombola.com>
2015-12-09 14:47:17 +0000

    got two more routes working to bring back random or indexed quote

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var app = express();
 
+var quotes = [
+    { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
+    { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
+    { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
+    { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
+];
+
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Hello World');
+    res.json(quotes);
+});
+
+app.get('/quote/random', function(req, res) {
+    var id = Math.floor(Math.random() * quotes.length);
+    var q = quotes[id];
+    res.json(q);
+});
+
+app.get('/quote/:id', function(req, res) {
+    if(quotes.length <= req.params.id || req.params.id < 0) {
+        res.statusCode = 404;
+        return res.send('Error 404: No quote found');
+    }
+    var q = quotes[req.params.id];
+    res.json(q);
 });
 
 app.listen(process.env.PORT || 4730);
